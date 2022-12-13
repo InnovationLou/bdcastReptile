6ad4f7fbbe4ab3471ac46cf7cfa01584b865b205
innovationLouke <lcklck1998@gmail.com>
2022-12-13 16:38:39 +0800

    更新请求

--- src/main/java/org/innovation/dybroadcastParser/dispatch/AudioDispatcher.java
+++ src/main/java/org/innovation/dybroadcastParser/dispatch/AudioDispatcher.java
@@ -11,21 +11,6 @@ import javax.crypto.SecretKey;
 public class AudioDispatcher {
 
     public void test(){
-        try {
-//            主账号ID 100012403366
-//            用户名 audioUser
-//            登录密码 %sluCQOl
-//            SecretId AKIDoU7b8TjohpoiS4Yszta5NRLi7NQgojT3
-//            SecretKey Vu9Azd5JJ7k9rPmyymNkwxmeNwLg91iF
-            Credential cred = new Credential("AKIDoU7b8TjohpoiS4Yszta5NRLi7NQgojT3", "Vu9Azd5JJ7k9rPmyymNkwxmeNwLg91iF");
-            CvmClient client = new CvmClient(cred, "ap-shanghai");
 
-            DescribeInstancesRequest req = new DescribeInstancesRequest();
-            DescribeInstancesResponse resp = client.DescribeInstances(req);
-
-            System.out.println(DescribeInstancesResponse.toJsonString(resp));
-        } catch (TencentCloudSDKException e) {
-            System.out.println(e.toString());
-        }
     }
 }
